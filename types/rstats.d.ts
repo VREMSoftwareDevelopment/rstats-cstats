2f49353c3717cc310cc6886a6bed66313eb6f262
awsr <43862868+awsr@users.noreply.github.com>
2023-12-15 18:57:15 -0800

    v2 log format

---
+++ types/rstats.d.ts
@@ -0,0 +1,56 @@
+export type RStats = {
+  meta: {
+    format: 2,
+    time_data: string,
+    time_script: string
+  },
+  daily: [
+    {
+      date: string,
+      traffic: [down: number, up: number],
+      error: [down: boolean, up: boolean]
+      misc?: {
+        [key: string]: any
+      }
+    }
+  ],
+  monthly: [
+    {
+      date: string,
+      traffic: [down: number, up: number],
+      error: [down: boolean, up: boolean]
+      misc?: {
+        [key: string]: any
+      }
+    }
+  ]
+}
+
+export type RStatsLegacy = {
+  meta: {
+    time_data: string,
+    time_script: string
+  },
+  daily: [
+    {
+      date: string,
+      down: number,
+      up: number,
+      comment?: {
+        message: string,
+        error_down: boolean,
+        error_up: boolean
+      }
+    }
+  ],
+  monthly: [
+    {
+      date: string,
+      down: number,
+      up: number,
+      comment?: {
+        message: string
+      }
+    }
+  ]
+}
--- util/interop.py
+++ util/interop.py
@@ -1,19 +1,73 @@
-def data_interop(data: dict, current_ver: int) -> str:
+import argparse
+import json
+
+
+CURRENT_VERSION = 2
+
+
+def v1_upgrade(data: dict):
+    """Convert file version 1"""
+    for idx in range(len(data["daily"])):
+        old_dict = data["daily"][idx]
+        new_dict = {
+            "date": old_dict["date"],
+            "traffic": [old_dict["down"], old_dict["up"]],
+            "error": [False, False],
+        }
+        if "comment" in old_dict:
+            err_down = False if old_dict["comment"]["cutoff_down"] is None else True
+            err_up = False if old_dict["comment"]["cutoff_up"] is None else True
+            new_dict["error"] = [err_down, err_up]
+            new_dict["misc"] = {
+                "comment": old_dict["comment"]["message"],
+                "cutoff_down": old_dict["comment"]["cutoff_down"],
+                "cutoff_up": old_dict["comment"]["cutoff_up"],
+            }
+        data["daily"][idx] = new_dict
+    for idx in range(len(data["monthly"])):
+        old_dict = data["monthly"][idx]
+        new_dict = {
+            "date": old_dict["date"],
+            "traffic": [old_dict["down"], old_dict["up"]],
+            "error": ["comment" in old_dict, False],
+        }
+        if "comment" in old_dict:
+            new_dict["misc"] = {"comment": old_dict["comment"]["message"]}
+        data["monthly"][idx] = new_dict
+    return data
+
+
+def data_interop(data: dict, current_ver: int) -> dict:
     try:
-        if "format" in data["meta"] and data["meta"]["format"] == current_ver:
-            return data
-        if "format" not in data["meta"]:
-            for idx in range(len(data["daily"])):
-                if "comment" in data["daily"][idx]:
-                    err_down = isinstance(
-                        data["daily"][idx]["comment"].pop("cutoff_down", None), str
-                    )
-                    err_up = isinstance(
-                        data["daily"][idx]["comment"].pop("cutoff_up", None), str
-                    )
-                    data["daily"][idx]["comment"]["error_down"] = err_down
-                    data["daily"][idx]["comment"]["error_up"] = err_up
-        return data
+        if "format" in data["meta"]:
+            data_ver = data["meta"]["format"]
+            if data_ver == current_ver:
+                return data
+            if data_ver == 1:
+                return v1_upgrade(data)
+            # Insert version numbers here
+            if data_ver > current_ver:
+                raise ValueError(
+                    f"Data format version {data_ver} > script format version {current_ver}"
+                )
+            raise ValueError(f"{data_ver} is not a valid version number")
+        # No format version info (version 1)
+        return v1_upgrade(data)
     except Exception as err:  # pylint: disable=broad-exception-caught
-        print(err)
+        print("Exception occured during data interop processing:\n", err)
         return data
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="RStats-Logger Interop",
+        description="Convert older JSON logs to newer format",
+    )
+    parser.add_argument("path", required=True)
+    args = parser.parse_args()
+
+    with open(args.path, "r", encoding="utf8") as f:
+        file_data = json.loads(f.read())
+    data_interop(file_data, CURRENT_VERSION)
+    with open(args.path, "w", encoding="utf8") as f:
+        f.write(json.dumps(file_data))
